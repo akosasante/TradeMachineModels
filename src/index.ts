cab57d4209b543239715afa61f4b5684136d2462
Akosua Asante <akosuaasante@gmail.com>
2019-11-03 21:03:50 -0500

    export index to make things accessible

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import DraftPick from './draftPick'
+import MajorLeaguePlayer from './majorLeaguePlayer'
+import MinorLeaguePlayer from './minorLeaguePlayer'
+import Player from './player'
+import Team from './team'
+import Trade from './trade'
+import TradeItem from './tradeItem'
+import User from './user'
+
+export default {
+    UserModel: User,
+    TradeModel: Trade,
+    TradeItemModel: TradeItem,
+    TeamModel: Team,
+    PlayerModel: Player,
+    MinorLeaguePlayerModel: MinorLeaguePlayer,
+    MajorLeaguePlayerModel: MajorLeaguePlayer,
+    DraftPickModel: DraftPick,
+}
