60538205e0b40a520fc2047eedadc4760eafad14
Akosua Asante <akosuaasante@gmail.com>
2019-11-29 19:54:19 -0500

    update the password field of this public user model

--- src/__tests__/mocks and factories/mockUserFactory.ts
+++ src/__tests__/mocks and factories/mockUserFactory.ts
@@ -2,16 +2,15 @@ import { name } from 'faker'
 import User from '../../models/user'
 import { getId } from '../sharedTestFunctions'
 
-const GENERIC_PASSWORD = 'roflpass';
 const ADMIN_NAME = `${name.findName()}`
 const ADMIN_EMAIL = `admin-${ADMIN_NAME}@example.com`
 const OWNER_NAME = `${name.findName()}`
 const OWNER_EMAIL = `owner-${OWNER_NAME}@example.com`
 
-export function createAdminUser(): User {
-    return new User(getId(), GENERIC_PASSWORD, ADMIN_NAME, ADMIN_EMAIL, 'admin')
+export function createAdminUser(hasPassword = true): User {
+    return new User(getId(), hasPassword, ADMIN_NAME, ADMIN_EMAIL, 'admin')
 }
 
-export function createOwnerUser(): User {
-    return new User(getId(), GENERIC_PASSWORD, OWNER_NAME, OWNER_EMAIL, 'owner')
+export function createOwnerUser(hasPassword = true): User {
+    return new User(getId(), hasPassword, OWNER_NAME, OWNER_EMAIL, 'owner')
 }
